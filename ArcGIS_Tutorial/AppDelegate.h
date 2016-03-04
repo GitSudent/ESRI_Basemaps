5885ba213d99e3b9438f1fc0acbb1bf1a4d62ab1
Charles Grier <chgrier@mac.com>
2016-03-04 08:20:40 -0500

    Initial Commit

---
+++ ArcGIS_Tutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArcGIS_Tutorial
+//
+//  Created by Charles Grier on 3/4/16.
+//  Copyright © 2016 Grier Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArcGIS_Tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArcGIS_Tutorial
+//
+//  Created by Charles Grier on 3/4/16.
+//  Copyright © 2016 Grier Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
